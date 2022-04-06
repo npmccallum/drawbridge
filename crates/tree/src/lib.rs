6890252dd0f2dc5e02039492bfca3ab1f3583712
Nathaniel McCallum <nathaniel@profian.com>
2022-04-06 10:57:53 -0400

    other misc changes
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- crates/tree/src/lib.rs
+++ crates/tree/src/lib.rs
@@ -4,102 +4,68 @@
 #![warn(rust_2018_idioms, unused_lifetimes, unused_qualifications, clippy::all)]
 #![forbid(unsafe_code)]
 
-mod meta;
-mod node;
-mod path;
-mod storage;
+use drawbridge_type::Entry;
+use drawbridge_type::Meta;
 
-pub use storage::Memory;
-
-use async_std::io::{copy, sink};
-use drawbridge_http::http::{Body, Error, Method, Request, Response, Result, StatusCode};
-use drawbridge_http::{async_trait, Handler, IntoResponse, Json};
-
-use self::meta::Meta;
-use self::node::Node;
-use self::path::Path;
-use self::storage::Storage;
-
-#[derive(Clone, Default)]
-pub struct Service<T: Clone + Storage>(T);
-
-impl<T: Clone + Storage> From<T> for Service<T> {
-    fn from(storage: T) -> Self {
-        Self(storage)
-    }
-}
-
-impl<T: Clone + Storage> Service<T> {
-    async fn roots(&self) -> Result<impl IntoResponse> {
-        self.0.roots().await.map(Json)
-    }
-
-    async fn options(&self, path: Path) -> Result<impl IntoResponse> {
-        self.0.wants(path).await.map(Json)
-    }
-
-    async fn delete(&self, path: Path) -> Result<impl IntoResponse> {
-        self.0.del(path).await
-    }
-
-    async fn head(&self, path: Path) -> Result<impl IntoResponse> {
-        self.0.get(path).await.map(|(meta, ..)| {
-            (
-                [
-                    ("Content-Length", meta.size.to_string()),
-                    ("Content-Type", meta.mime.to_string()),
-                ],
-                (),
-            )
-        })
-    }
-
-    async fn get(&self, path: Path) -> Result<impl IntoResponse> {
-        self.0.get(path).await.map(|(meta, data)| {
-            (
-                [
-                    ("Content-Length", meta.size.to_string()),
-                    ("Content-Type", meta.mime.to_string()),
-                ],
-                data,
-            )
-        })
-    }
-
-    async fn put(&self, path: Path, meta: Meta, body: Body) -> Result<impl IntoResponse> {
-        // Validate that the final Node is the measurement of the Meta.
-        let buf = serde_json::to_vec(&meta)
-            .map_err(|_| Error::from_str(StatusCode::InternalServerError, ""))?;
-        let mut rdr = (*path[path.len() - 1]).clone().reader(&buf[..]);
-        copy(&mut rdr, &mut sink())
-            .await
-            .map_err(|_| Error::from_str(StatusCode::BadRequest, ""))?;
-
-        // Validate the measurement of the body as it is read.
-        let body = (*meta.hash).clone().reader(body);
-
-        self.0.put(path, meta, body).await
-    }
-}
+use axum::async_trait;
+use axum::body::HttpBody;
+use axum::body::StreamBody;
+use axum::extract::{Extension, FromRequest, Path};
+use axum::response::Error;
+use axum::Router;
+use tokio::io::AsyncRead;
+use tokio_util::io::ReaderStream;
 
 #[async_trait]
-impl<T: Clone + Storage> Handler<()> for Service<T> {
-    type Response = Response;
+pub trait Storage: Send {
+    type Body: 'static + Send + AsyncRead;
 
-    async fn handle(self, req: Request) -> Result<Self::Response> {
-        let path = req.url().path().trim_start_matches('/');
-        let meth = req.method();
+    async fn get(&self, path: &[&str]) -> Result<(Meta, Self::Body), Error>;
+    async fn put<T>(&self, path: &[&str], meta: Meta, body: T) -> Result<(), Error>
+    where
+        T: Send + AsyncRead + Unpin;
+}
 
-        match (path, meth) {
-            ("", Method::Get) => (|| self.roots()).handle(req).await,
+pub fn app<T: Storage, B: HttpBody>() -> Router<B>
+where
+    Extension<T>: FromRequest<B>,
+    T: 'static + Send,
+    B: 'static + Send,
+{
+    use axum::routing::*;
 
-            (.., Method::Options) => (|p| self.options(p)).handle(req).await,
-            (.., Method::Delete) => (|p| self.delete(p)).handle(req).await,
-            (.., Method::Head) => (|p| self.head(p)).handle(req).await,
-            (.., Method::Get) => (|p| self.get(p)).handle(req).await,
-            (.., Method::Put) => (|p, m, b| self.put(p, m, b)).handle(req).await,
+    Router::new()
+        .route("/*path", put(|s, e, p, m| self::put::<T>(s, e, p, m)))
+        .route("/*path", head(|s, e, p| self::head::<T>(s, e, p)))
+        .route("/*path", get(|s, e, p| self::get::<T>(s, e, p)))
+}
 
-            _ => Err(Error::from_str(StatusCode::MethodNotAllowed, "")),
-        }
-    }
+async fn head<T: Storage>(
+    Extension(storage): Extension<T>,
+    Extension(entry): Extension<Entry>,
+    Path(path): Path<String>,
+) -> Result<Meta, Error> {
+    let path: Vec<&str> = path.trim_start_matches('/').split('/').collect();
+    let (meta, ..) = storage.get(&path).await?;
+    Ok(meta)
+}
+
+async fn put<T: Storage>(
+    Extension(storage): Extension<T>,
+    Extension(entry): Extension<Entry>,
+    Path(path): Path<String>,
+    meta: Meta,
+) -> Result<(), Error> {
+    let path: Vec<&str> = path.trim_start_matches('/').split('/').collect();
+    Ok(())
+}
+
+async fn get<T: Storage>(
+    Extension(storage): Extension<T>,
+    Extension(entry): Extension<Entry>,
+    Path(path): Path<String>,
+) -> Result<(Meta, StreamBody<ReaderStream<T::Body>>), Error> {
+    let path: Vec<&str> = path.trim_start_matches('/').split('/').collect();
+    let (meta, data) = storage.get(&path).await?;
+    Ok((meta, StreamBody::new(ReaderStream::new(data))))
 }
